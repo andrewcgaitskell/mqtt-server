ca26f5389392b9ceed9e3739291a50283f5dcaa0
andrewcgaitskell <andrew@gaitskell.com>
2020-11-06 11:53:54 +0000

    Update installpostgrs.sh

--- installpostgrs.sh
+++ installpostgrs.sh
@@ -1,21 +1,22 @@
-apt update
-apt upgrade -y
+sudo apt update
+sudo apt upgrade -y
 
-apt install sudo
+sudo apt install sudo
 
-apt install -y software-properties-common
-apt install -y postgresql-10
-apt install -y postgresql-client-10
-postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
+sudo apt install -y software-properties-common
+sudo apt install -y postgresql-10
+sudo apt install -y postgresql-client-10
 
-postgres psql -c "CREATE EXTENSION adminpack;"
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
+
+sudo -u postgres psql -c "CREATE EXTENSION adminpack;"
 
 psqluser="pythonuser"   # Database username
 psqlpass="pythonuser"  # Database password
 psqldb="data"   # Database name
 
-printf "CREATE ROLE pythonuser WITH LOGIN SUPERUSER INHERIT CREATEDB CREATEROLE NOREPLICATION PASSWORD 'pythonuser';\nCREATE DATABASE $psqldb WITH OWNER $psqluser;" > createuserdb.sql
+sudo printf "CREATE ROLE pythonuser WITH LOGIN SUPERUSER INHERIT CREATEDB CREATEROLE NOREPLICATION PASSWORD 'pythonuser';\nCREATE DATABASE $psqldb WITH OWNER $psqluser;" > createuserdb.sql
 
-postgres psql -f createuserdb.sql
+sudo -u postgres psql -f createuserdb.sql
 
-apt install postgis
+sudo apt install postgis
