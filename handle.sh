3620c5e31d1b0e90d4bb127b8fd77b055529cb31
andrewcgaitskell <andrew@gaitskell.com>
2020-11-06 13:50:29 +0000

    Update handle.sh

--- handle.sh
+++ handle.sh
@@ -5,3 +5,8 @@ sudo printf "COPY "raw".data_device_tbl FROM MQTT-Values.csv;" > poptable.sql
 
 sudo -u postgres psql -f poptable.sql
 
+
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
+
+sudo -u postgres psql -c "COPY raw.data_device_tbl FROM MQTT-Values.csv;"
+
