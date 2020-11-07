6c610bf5dc4a3ef8d73d9c3bd3a204eb17f42e1b
andrewcgaitskell <andrew@gaitskell.com>
2020-11-07 10:08:09 +0000

    Update virtualenv.sh

--- virtualenv.sh
+++ virtualenv.sh
@@ -6,4 +6,24 @@ pip install wheel
 pip install uwsgi
 pip install Flask
 
+hello world app
+===============
 
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+uwsgi
+======
+
+uwsgi -s /tmp/yourapplication.sock --manage-script-name --mount /yourapplication=myapp:app
+
+--virtualenv /path/to/virtual/environment.
+
+uwsgi -s /tmp/yourapplication.sock --manage-script-name --mount /yourapplication=hello:app --virtualenv /home/andrew_gaitskell/env
+
+
+You might also need to add --plugin python or --plugin python3 depending on which python version you use for your project.
