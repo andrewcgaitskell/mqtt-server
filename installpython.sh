af914bc0cc85480e88ef859c9043d6cfbbfcb9f2
andrewcgaitskell <andrew@gaitskell.com>
2020-11-07 09:35:40 +0000

    Update installpython.sh

--- installpython.sh
+++ installpython.sh
@@ -1,11 +1,11 @@
-sudo apt install python-software-properties
+sudo apt install python-software-properties -Y
 
-sudo apt install software-properties-common
+sudo apt install software-properties-common -Y
 
-sudo apt install python3
-sudo apt install python3-dev
-sudo apt install libpq-dev
+sudo apt install python3 -Y
+sudo apt install python3-dev -Y
+sudo apt install libpq-dev -Y
 
-sudo apt install python3-pip
+sudo apt install python3-pip -Y
 
-sudo apt install python3-venv
+sudo apt install python3-venv -Y
